df873b6826ad91ed9c0558eea8dd3adf6815aab1
Juan Desimoni <desijuan89@gmail.com>
2022-08-29 13:42:52 -0300

    primera versión de NewGroup funcionando - small fix

--- src/views/GroupOverview.jsx
+++ src/views/GroupOverview.jsx
@@ -1,3 +1,8 @@
+import axios from 'axios';
+
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Window from './Window';
 
 // const ToSettleTheDebts = () => {
@@ -29,18 +34,80 @@ import Window from './Window';
 // };
 
 const Expenses = () => {
+  const [state, setState] = useState({ loading: true, data: {} });
+
+  const navigate = useNavigate();
+
+  const getGroup = async (groupID) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_SERVER_URL}/api/v1/groups/${groupID}`
+      );
+      setState({ loading: false, data: response.data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const groupID = '630a3f7bda370e75891e6020';
+
+  if (state.loading) {
+    getGroup(groupID);
+    return (
+      <div className='d-flex align-items-center justify-content-between'>
+        <span id='expenses-status-text'>Loading...</span>
+        <div
+          className='spinner-border spinner-border-sm'
+          role='status'
+          aria-hidden='true'
+        ></div>
+      </div>
+    );
+  }
+
+  const group = state.data;
+
+  if (group.ledger.length === 0) {
+    return (
+      <div>
+        <div className='d-flex align-items-center justify-content-between'>
+          <h4>Expenses</h4>
+          <button
+            type='button'
+            className='btn btn-primary btn-sm'
+            id='new-expense-button'
+            onClick={() => navigate('new-expense')}
+          >
+            New expense
+          </button>
+        </div>
+
+        <span>No expenses yet.</span>
+      </div>
+    );
+  }
+
   return (
     <>
-      <h4>Expenses</h4>
-      <button
-        type='button'
-        className='btn btn-primary btn-sm float-end'
-        id='new-expense-button'
-      >
-        New expense
-      </button>
-      <span id='expenses-status-text'>Loading...</span>
-      <ul id='expenses-list' hidden></ul>
+      <div className='d-flex align-items-center justify-content-between'>
+        <h4>Expenses</h4>
+        <button
+          type='button'
+          className='btn btn-primary btn-sm'
+          id='new-expense-button'
+          onClick={() => navigate('new-expense')}
+        >
+          New expense
+        </button>
+      </div>
+
+      <ul id='expenses-list'>
+        {group.ledger.map((entry) => (
+          <li>{`${entry.from} gave $${entry.amount.toFixed(2)} to ${
+            entry.to
+          } for ${entry.for}.`}</li>
+        ))}
+      </ul>
     </>
   );
 };
