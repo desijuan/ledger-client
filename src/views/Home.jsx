834236ef0b462c436dfa73754a7389164754e366
Juan Desimoni <desijuan89@gmail.com>
2022-09-19 16:16:02 -0300

    nicer Home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
 
 const Home = () => (
-  <div className='container text-center'>
-    <h1 className='my-4'>Ledger app</h1>
-    <Link to='/new-group' className='btn btn-primary btn-lg'>
-      New group
-    </Link>
+  <div className='container my-3'>
+    <div className='card mx-auto shadow-sm'>
+      <div className='card-body text-center'>
+        <div className='my-3'>
+          <h1 className='mb-4'>Ledger App</h1>
+          <Link to='/new-group' className='btn btn-primary btn-lg'>
+            New group
+          </Link>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
